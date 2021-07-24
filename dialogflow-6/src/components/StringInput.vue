cf351f2ae59b627f246f6d51347c928fddec0024
Khaleel Gibran <khalby786@gmail.com>
2021-07-24 21:15:46 +0530

    (feat) display sentiment values

--- dialogflow-6/src/components/StringInput.vue
+++ dialogflow-6/src/components/StringInput.vue
@@ -1,37 +1,65 @@
 <template>
-  <form
-    @submit.prevent="getSentiment()"
-    class="sentiment-input-form"
-    v-if="showInput === true"
-  >
-    <label for="sentiment-text">so tell me, how do you feel right now?</label>
-    <textarea
-      id="sentiment-text"
-      v-model="sentimentText"
-      rows="5"
-      cols="50"
-      maxlength="500"
-      placeholder="Type here..."
-      required
-    ></textarea>
-    <!-- <input type="submit" class="sentiment-input-submit" value="Submit" /> -->
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+  <div v-if="showInput === true">
+    <form @submit.prevent="getSentiment()" class="sentiment-input-form">
+      <label for="sentiment-text">so tell me, how do you feel right now?</label>
+      <textarea
+        id="sentiment-text"
+        v-model="sentiment.sentimentText"
+        rows="5"
+        cols="50"
+        maxlength="500"
+        placeholder="Type here..."
+        required
+      ></textarea>
+      <!-- <input type="submit" class="sentiment-input-submit" value="Submit" /> -->
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <div class="results" v-if="showOutput === true">
+      <p
+        :style="{
+          textTransform: 'uppercase',
+          fontSize: '12px',
+          marginBottom: '0px',
+        }"
+      >
+        STATS FOR NERDS
+      </p>
+      <p class="magnitude">
+        Magnitude: {{ sentiment.sentimentOutput.queryTextSentiment.magnitude }}
+      </p>
+      <p class="score">
+        Score: {{ sentiment.sentimentOutput.queryTextSentiment.score }}
+      </p>
+      <p class="finalEmotion">It looks like you're {{ sentiment.finalEmotion }}</p>
+    </div>
+    <TextLoading :showLoadingOverlay="showTextLoading" />
+  </div>
 </template>
 
 <script>
 import DOMPurify from "dompurify";
 
+import TextLoading from "@/components/TextLoading.vue";
+
 export default {
   name: "StringInput",
   data() {
     return {
-      sentimentText: null,
+      sentiment: {
+        sentimentText: null,
+        sentimentOutput: null,
+        finalEmotion: null,
+      },
+      showOutput: false,
+      showTextLoading: false,
     };
+  },
+  components: {
+    TextLoading,
   },
   computed: {
     cleanedSentimentText: function () {
-      return DOMPurify.sanitize(this.sentimentText);
+      return DOMPurify.sanitize(this.sentiment.sentimentText);
     },
   },
   props: {
@@ -41,17 +69,36 @@ export default {
     },
   },
   methods: {
-    getSentiment() {
-      (async () => {
-        // fetch backend API to get sentiment analysis
-        let sentiment = await fetch(
-          "/api/getSentimentValue?query=" +
-            encodeURIComponent(this.cleanedSentimentText)
-        );
-        let response = await sentiment.json();
+    getSentiment: async function () {
+      // show loading stuff
+      this.showTextLoading = true;
 
-        console.log(response);
-      })();
+      // fetch backend API to get sentiment analysis
+      let sentiment = await fetch(
+        "/api/getSentimentValue?query=" +
+          encodeURIComponent(this.cleanedSentimentText)
+      );
+      let response = await sentiment.json();
+      this.sentiment.sentimentOutput = response;
+      this.showOutput = true;
+      this.showTextLoading = false;
+
+      if (
+        response.queryTextSentiment.score > 0 &&
+        response.queryTextSentiment.score < 0.5
+      ) {
+        this.sentiment.finalEmotion = "both happy and sad?";
+      } else if (response.queryTextSentiment.score > 0.5) {
+        this.sentiment.finalEmotion = "definitely feeling joyful!";
+      } else if (
+        response.queryTextSentiment.score < 0 &&
+        response.queryTextSentiment.score > -0.5
+      ) {
+        this.sentiment.finalEmotion = "feeling both happy and sad! Cheer up!";
+      } else if (response.queryTextSentiment.score < -0.5) {
+        this.sentiment.finalEmotion =
+          "feeling down, but nothing that a meme can't improve!";
+      }
     },
   },
 };
@@ -83,5 +130,10 @@ textarea {
   padding: 20px 20px;
   border: 1px solid var(--rosy-brown);
   border-radius: 3px;
+}
+
+.magnitude,
+.score {
+  margin: 0px;
 }
 </style>
