91cd638473e9100bb002678acef2f658a38222c7
Khaleel Gibran <khalby786@gmail.com>
2021-07-24 18:32:33 +0530

    (feat) basic vue app, input form

--- dialogflow-6/src/App.vue
+++ dialogflow-6/src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>mood-o-(meme)ter</h1>
+    <div class="description">
+      <p>your current mood based on memes</p>
+    </div>
+    <StringInput />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StringInput from "@/components/StringInput.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    StringInput: StringInput,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap");
+
+:root {
+  --sans: "Inter", "Helvetica", sans-serif;
+  --baby-blue-eyes: #9ac4f8ff;
+  --magic-mint: #99edccff;
+  --rosy-brown: #cb958eff;
+  --blush: #e36588ff;
+  --jazzberry-jam: #9a275aff;
+}
+
+body {
+  font-family: var(--sans);
+  margin: 0 auto;
+  max-width: 700px;
+  background-color: var(--baby-blue-eyes);
+  padding-top: 40px;
+  padding-bottom: 40px;
+}
+
+h1 {
+  color: var(--jazzberry-jam);
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 0px;
+}
+
+button {
+  display: block;
+  margin-top: 15px;
+  box-shadow: none;
+  color: white;
+  font-family: var(--sans);
+  transition: 150ms;
+  padding: 10px 20px;
+  cursor: pointer;
+  background-color: var(--blush);
+  border-radius: 3px;
+  font-size: 14px;
+  border: none;
+  box-shadow: 0 2.3px 1.9px rgba(0, 0, 0, 0.013),
+    0 5.4px 4.3px rgba(0, 0, 0, 0.019), 0 9.7px 7.7px rgba(0, 0, 0, 0.023),
+    0 16.1px 12.8px rgba(0, 0, 0, 0.027), 0 26.5px 21.2px rgba(0, 0, 0, 0.031),
+    0 46.2px 37px rgba(0, 0, 0, 0.037), 0 100px 80px rgba(0, 0, 0, 0.05);
+  transition: 200ms;
+}
+
+button:hover {
+  box-shadow: none;
+  transition: 200ms;
+}
+
+.description p {
+  text-align: center;
+  font-size: 1.5em;
+  margin-top: 0px;
 }
 </style>
---
+++ dialogflow-6/src/components/StringInput.vue
@@ -0,0 +1,61 @@
+<template>
+  <form @submit.prevent="getSentiment()" class="sentiment-input-form">
+    <label for="sentiment-text">so tell me, how do you feel right now?</label>
+    <textarea
+      id="sentiment-text"
+      v-model="sentimentText"
+      rows="5"
+      cols="50"
+      maxlength="500"
+      placeholder="Type here..."
+    ></textarea>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
+
+<script>
+import DOMPurify from "dompurify";
+
+export default {
+  name: "StringInput",
+  data() {
+    return {
+      sentimentText: null,
+    };
+  },
+  computed: {
+    cleanedSentimentText: function () {
+      return DOMPurify.sanitize(this.sentimentText);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sentiment-input-form {
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+.sentiment-input-form > * {
+  display: block;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+textarea {
+  display: block;
+  box-shadow: none;
+  border: none;
+  font-size: 72px;
+  color: var(--black);
+  transition: 150ms;
+  width: 100%;
+  background-color: var(--lightpink);
+  font-size: 24px;
+  font-family: var(--sans);
+  padding: 20px 20px;
+  border: 1px solid var(--rosy-brown);
+  border-radius: 3px;
+}
+</style>
