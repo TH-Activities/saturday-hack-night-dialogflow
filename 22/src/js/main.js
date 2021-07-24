a2b727ccff7b185b2c41c192638867b378698795
Sudev Suresh S <sudevssuresh@gmail.com>
2021-07-24 23:19:21 +0530

    Fetches mood and uses in api

--- 22/src/js/main.js
+++ 22/src/js/main.js
@@ -11,6 +11,18 @@ user_form.onsubmit = async (e) => {
   e.preventDefault();
 
   // Put API fetch requests here onwards
-  let response = apiReq("happy", user_input.value);
+
+  // Mood response
+  let mood;
+  fetch(`https://defiant-tabby-climb.glitch.me/meme/"${user_input.value}"`)
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      mood = data;
+    });
+
+  // Meme response
+  let response = apiReq(mood, user_input.value);
   console.log(response);
 };
