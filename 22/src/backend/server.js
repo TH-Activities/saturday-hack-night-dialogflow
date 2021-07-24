45d87d4146e788ebaa7c6fb1935b84f519583448
Ajay Krishna K V <55079486+AJAYK-01@users.noreply.github.com>
2021-07-24 22:27:18 +0530

    add glitch backend

---
+++ 22/src/backend/server.js
@@ -0,0 +1,40 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const app = express();
+const { executeQueries, detectIntent } = require('./df');
+
+// our default array of dreams
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+// send the default array of dreams to the webpage
+app.get("/meme/:q", (request, response) => {
+  // express helps us take JS objects and send them as JSON
+  let query = [request.params.q]
+  executeQueries('moodmeme-9cts', 'onesession', query, 'en-US').then((resp) => {
+    console.log(resp);
+    response.json(resp);
+  });
+  
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
