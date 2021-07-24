1b07c1b0a82124a9f6a22155274fde4a9deed83d
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-07-24 20:27:31 +0530

    dialogflow init

--- Dialogflow-18/index.js
+++ Dialogflow-18/index.js
@@ -1,1 +1,75 @@
 const dialogflow = require('@google-cloud/dialogflow');
+require('dotenv').config();
+
+const projectId = process.env.projectId;
+console.log(projectId)
+const sessionId = '123456';
+const queries = ['age']
+
+const languageCode = 'en';
+
+const sessionClient = new dialogflow.SessionsClient({
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+});
+
+async function detectIntent(
+    projectId,
+    sessionId,
+    query,
+    contexts,
+    languageCode
+) {
+    // The path to identify the agent that owns the created intent.
+    const sessionPath = sessionClient.projectAgentSessionPath(
+        projectId,
+        sessionId
+    );
+
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: query,
+                languageCode: languageCode,
+            },
+        },
+    };
+
+    if (contexts && contexts.length > 0) {
+        request.queryParams = {
+            contexts: contexts,
+        };
+    }
+
+    const responses = await sessionClient.detectIntent(request);
+    return responses[0];
+}
+
+async function executeQueries(projectId, sessionId, queries, languageCode) {
+    // Keeping the context across queries let's us simulate an ongoing conversation with the bot
+    let context;
+    let intentResponse;
+    for (const query of queries) {
+        try {
+            console.log(`Sending Query: ${query}`);
+            intentResponse = await detectIntent(
+                projectId,
+                sessionId,
+                query,
+                context,
+                languageCode
+            );
+            console.log('Detected intent');
+            console.log(
+                `Fulfillment Text: ${intentResponse.queryResult.fulfillmentText}`
+            );
+            // Use the context from this response for next queries
+            context = intentResponse.queryResult.outputContexts;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+executeQueries(projectId, sessionId, queries, languageCode);
