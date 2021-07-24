fba416c27fd368bc25f96ea3984924ad0f3469e3
Anbarasu <anbarasun123@gmail.com>
2021-07-24 22:46:18 +0530

    Can do Subtraction

--- Dialogflow-21/function/src/index.js
+++ Dialogflow-21/function/src/index.js
@@ -15,10 +15,18 @@ app.post('/',express.json(),(req,res) =>{
         const sum = num1 + num2;
         agent.add(sum + ' ');
     }
+    function subtraction(agent){
+        const num1 = agent.parameters.number1;
+        const num2 = agent.parameters.number2;
+        const difference = num1 - num2;
+        agent.add(difference + ' ');
+    }
 
 
     var intentMap = new Map();
     intentMap.set('Addition',addition);
+    intentMap.set('Subtraction',subtraction);
+
 
     agent.handleRequest(intentMap);
 })
